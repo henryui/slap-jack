5405fe9203876987dba7266217aeaed06afb426f
Yunsung Oh <henry@brokerbay.ca>
2024-08-18 22:57:44 -0400

    feat: add temp cron

---
+++ src/server/services/CronJobService.ts
@@ -0,0 +1,17 @@
+// import { CronJob } from 'cron';
+
+// const SYSTEM_TZ = 'America/Toronto';
+
+// class CronJobService {
+//   public startCronJob(time: string, fcn: Function) {
+//     new CronJob(
+//       time,
+//       fcn,
+//       null,
+//       true,
+//       SYSTEM_TZ,
+//     );
+//   }
+// }
+
+// export default new CronJobService();
